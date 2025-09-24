97a025b295ade2241b8c1cbb380c1f4c8f342bf8
Aditya Patidar <aditya.patidar@kadellabs.com>
2025-09-24 12:01:49 +0530

    base layouts updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,9 @@
-// App.js
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Mvp from "./pages/Mvp";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
-  return (
-    <Routes>
-      {/* Default route */}
-      <Route path="/" element={<Home />} />
-      <Route path="/mvp" element={<Mvp />} />
-      
-      {/* Placeholder routes for testing */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-  );
+  return <RouterProvider router={AppRoutes} />;
 }
 
 export default App;
